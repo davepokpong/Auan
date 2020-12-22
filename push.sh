727baeac05e15fb4d134cb39d732909875c809c4
nutchanonc <edu.nutchanon@gmail.com>
2020-12-22 20:05:56 +0700

    beam update the work

--- push.sh
+++ push.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-echo "Pulling the latest version of this repositoy............"
+echo "Pulling the latest version of this repository..."
 git pull origin main
-echo "commit files as $1"
+echo "commit files as <$1>"
+echo "Pushing to the repository..."
 git add .
 git commit -m "$1"
 git push -u origin main
