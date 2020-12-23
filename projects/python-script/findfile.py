972df2280ac2d73918fb7ba48953bff746163b21
nutchanonc <edu.nutchanon@gmail.com>
2020-12-23 13:11:35 +0700

    new version of findfile.py

--- projects/python-script/findfile.py
+++ projects/python-script/findfile.py
@@ -9,16 +9,16 @@ def Traverse(root,filename,count,searchtype):
     allfiles = [f for f in os.listdir(mypath)]
     onlyfiles = [fi for fi in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, fi))]
     onlydir = [d for d in os.listdir(mypath) if not os.path.isfile(os.path.join(mypath, d))]
-    if (searchtype == 'dir'):
+    if (searchtype == '-dir'):
         if (filename in onlydir):
             print(f"Found (dir): {mypath}/{filename}")
             count += 1
-    elif (searchtype == "type"):
+    elif (searchtype == "-ftype"):
         for k in allfiles:
             if (filename in k):
-                print(f"Found(file): {mypath}/{k}")
+                print(f"Found(file-type): {mypath}/{k}")
                 count += 1
-    else:
+    elif (searchtype == "-file"):
         for i in onlyfiles:
             if (i == filename):
                 print(f"Found (file): {mypath}/{filename}")
@@ -32,12 +32,10 @@ if (__name__ == "__main__"):
     os.chdir("../..")
     count = 0
     filename = sys.argv[1]
-    if (len(sys.argv) >= 3):
-        searchtype = sys.argv[2]
-    else:
-        searchtype = "file"
+    searchtype = sys.argv[2]
+
     if (len(sys.argv) >= 4):
-        root = os.path.abspath(os.getcwd())+'/'+sys.argv[3]
+        root = os.path.abspath(os.getcwd())+sys.argv[3]
     else:
         root = os.path.abspath(os.getcwd())
 
@@ -48,6 +46,7 @@ if (__name__ == "__main__"):
     if (count == 0):
         print(f"No file found under {root}")
     else:
-        print(f"Find {count} file(s) under dir: {root}")
+        print()
+        print(f"Totally Found {count} file(s).")
     print()
     print("="*60)
