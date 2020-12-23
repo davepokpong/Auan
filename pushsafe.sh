7d92afabc97f91d10309012aad36d8bac7621993
nutchanonc <edu.nutchanon@gmail.com>
2020-12-23 21:12:01 +0700

    Full version

--- pushsafe.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Adding and committing all updated files."
-echo "===================   Safe Push ========================="
-git add .
-git commit -m "$1"
-git push -u origin main
-echo "===================   Done      ========================="
